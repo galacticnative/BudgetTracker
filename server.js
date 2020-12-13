ea1106b1435b1525f5b7677e8f3df5c4362f03d1
galacticnative <68270151+galacticnative@users.noreply.github.com>
2020-12-13 11:59:33 -0700

    update package json error

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require("express");
-//const logger = require("morgan");
+const logger = require("morgan");
 const mongoose = require("mongoose");
-//const compression = require("compression");
+const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 const app = express();
 
-//app.use(logger("dev"));
+app.use(logger("dev"));
 
-//app.use(compression());
+app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -25,5 +25,5 @@ mongoose.connect(MONGODB_URI, {
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log(`App running on port ${PORT}! 👽`);
 });
